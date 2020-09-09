bf14225146aedbf99980e95f2dfb66e992688fe9
NatrezC <cnatrez@gmail.com>
2020-09-08 22:11:12 -0500

    forgot to commit each step

--- dom_practice.js
+++ dom_practice.js
@@ -1,1 +1,13 @@
-console.log("hi")
+//console.log("hi")
+const middle = document.querySelector("div")
+//console.log(middle)
+const h2 = document.querySelector("h2")
+//console.log(h2)
+h2.innerText = "Welcome to Memphis!!!"
+const list = document.querySelectorAll("li")
+//console.log(list)
+list[2].style.color = "red"
+list[1].innerText = "Jamboree sandwich at the Jambo-ree"
+// const Memphis = document.createElement("Memphis")
+// Memphis.setAttribute("src", "https://i.imgur.com/t34hgq2.jpg")
+// middle[2].appendChild("Memphis")
